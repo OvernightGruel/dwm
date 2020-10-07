bee36e4c1a105aa6367f9ba70f720a535a705a4d
OvernightGruel <clayzwq@gmail.com>
2020-10-07 12:21:38 +0800

    vol settings

--- config.h
+++ config.h
@@ -1,23 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const int viewontag          = 1;     /* Switch view on tag switch */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "MesloLGS NF:size=16" };
+static const char dmenufont[]       = "MesloLGS NF:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#73575c";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -71,7 +71,10 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *upvol[]   = { "/home/clay/dwm/dwm-6.2/scripts/vol-up.sh",  NULL };
+static const char *downvol[] = { "/home/clay/dwm/dwm-6.2/scripts/vol-down.sh",  NULL };
+static const char *mutevol[] = { "/home/clay/dwm/dwm-6.2/scripts/vol-toggle.sh",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -84,6 +87,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      focusstackhid,  {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_k,      rotatestack,    {.i = -1 } },
+	{ ControlMask,                  XK_d,      spawn,          {.v = downvol } },
+	{ ControlMask,                  XK_m,      spawn,          {.v = mutevol } },
+	{ ControlMask,                  XK_u,      spawn,          {.v = upvol   } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
---
+++ scripts/system_status.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+week=$(date '+%A')
+case "$week" in
+	"Monday") week_cn="一" ;;
+	"Tuesday") week_cn="二" ;;
+	"Wednesday") week_cn="三" ;;
+	"Thursday") week_cn="四" ;;
+	"Friday") week_cn="五" ;;
+	"Saturday") week_cn="六" ;;
+	"Sunday") week_cn="日" ;;
+esac
+localtime=$(date +"%Y-%m-%d(${week_cn})%H:%M")
+
+vol=$(amixer get Master | tail -n1 | sed -r "s/.*\[(.*)%\].*/\1/")
+
+hdfree=$(df -h /dev/nvme0n1p3 | awk ' /[0-9]/ {print $3 "/" $2}')
+
+memfree=$(free --mebi | sed -n '2{p;q}' | awk '{printf ("%2.2f%\n", (( $3 / $2 ) * 100))}')
+
+# cpu
+
+xsetroot -name " ${memfree} | ${hdfree} | ${vol} | ${localtime} "
